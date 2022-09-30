a3c0dd2b3e1c5af84b07dc002300258ba24748bd
Alexandra Kropf <kropfgames@gmail.com>
2022-09-30 12:43:25 -0700

    Build game object for triggering mobile keyboard
    Broken - mobile keyboard isn't sending input to input field. Must add more code.

--- Assets/TriggerMobileKeyboard.cs
+++ Assets/TriggerMobileKeyboard.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.EventSystems;// Required when using Event data.
+using UnityEngine; 
 using TMPro;
 
-public class TriggerMobileKeyboard : MonoBehaviour, 
-ISelectHandler// required interface when using the OnSelect method.
+public class TriggerMobileKeyboard : MonoBehaviour 
 {
    [SerializeField] public TMP_InputField InputField;
- 
+    private TouchScreenKeyboard keyboard;
+
     //Do this when the selectable UI object is selected.
-    public void OnSelect(BaseEventData eventData)
-    {  
-            Debug.Log("InputField.isFocused");
-            TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);    
-    }
+    public void OpenMobileKeyboard()  {  
+           Debug.Log("OnSelect input");
+           keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);     
+    } 
+    public void ShowMobileInputInInputField() { //will break the input on desktop. only works on mobile.
+        InputField.text = keyboard.text;
+        Debug.Log("keyboard.text " + keyboard.text);
+        Debug.Log("InputField.text " + InputField.text); 
+    } 
 }
